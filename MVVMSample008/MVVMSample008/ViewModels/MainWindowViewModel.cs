a3dfac304732628a4d52661b0154ec9795c87c0f
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-05-06 07:03:39 +0900

    Add files via upload

---
+++ MVVMSample008/MVVMSample008/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Input;
+
+namespace MVVMSample008.ViewModels
+{
+    public class MainWindowViewModel : ObservableValidator
+    {
+        private string _fullName;
+        [Required(ErrorMessage = "{0} is a required field.")]
+        [StringLength(20, ErrorMessage = "Character length is too long.")]
+        public string FullName
+        {
+            get => _fullName;
+            set => SetProperty(ref _fullName, value);
+        }
+
+        private int _age;
+        public int Age
+        {
+            get => _age;
+            set => SetProperty(ref _age, value);
+        }
+
+        private string _note;
+        public string Note
+        {
+            get => _note;
+            set => SetProperty(ref _note, value);
+        }
+
+        public bool IsNormal { get => !this.HasErrors; }
+
+        public ICommand ValidateCommand { get; }
+        public ICommand ClearErrorCommand { get; }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            ValidateCommand = new RelayCommand(() =>
+            {
+                this.ValidateAllProperties();
+            });
+
+            ClearErrorCommand = new RelayCommand(() =>
+            {
+                this.ClearErrors();
+            });
+        }
+    }
+}
---
+++ MVVMSample008/MVVMSample008/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MVVMSample008.ViewModels;
+using System.Windows;
+
+namespace MVVMSample008.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
